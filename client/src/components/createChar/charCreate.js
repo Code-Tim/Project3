9b9315b6d1c9de7a4f540c1be2d3edd6bf3e1e5d
Code-Tim <62041831+Code-Tim@users.noreply.github.com>
2020-08-29 15:17:12 -0400

    added create char page

---
+++ client/src/components/createChar/charCreate.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./charCreate.css";
+// need main game page linked
+
+// The ...props means, spread all of the passed props onto this element
+// That way we don't have to define them all individually
+function CharCreate() {
+    return (
+
+        <div className="CreateChar">
+            <h1>Character Creation</h1>
+
+    this.state = {
+                playerName : '',
+			HP: 50,
+			Stamina : 60,
+			Atk : 10,
+			Def : 10
+		};
+
+		this.handleChangeName = this.handleChangeName.bind(this);
+		this.changeHP = this.changeHP.bind(this);
+		this.takeDamage = this.takeDamage.bind(this);
+		this.doDamage = this.doDamage.bind(this);
+
+        </div>
+    )
+};
+
+export default CharCreate;
