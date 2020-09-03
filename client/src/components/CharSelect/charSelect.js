b2ecb5e0db3ec0b8a300333bdaa386ee1c61e54c
Code-Tim <62041831+Code-Tim@users.noreply.github.com>
2020-09-02 20:29:00 -0400

    new merge

--- client/src/components/CharSelect/charSelect.js
+++ client/src/components/CharSelect/charSelect.js
@@ -1,12 +1,6 @@
 import React from "react";
 // import "./charSelect.css";
 
-// import { browserRouter as Router, Route, Switch } from "react-router-dom";
-// import Routes from './Routes';
-// need main game page linked
-
-// The ...props means, spread all of the passed props onto this element
-// That way we don't have to define them all individually
 function CharSelect() {
     return (
 
